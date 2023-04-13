37a9f2e82da29ce1c8d343f0431e46a362e9c663
Ismael Martín <alu0101397375@ull.edu.es>
2023-04-13 10:31:41 +0000

    ejercicio 3 en proceso

--- src/ejercicio-3/cliente-servidor/client.ts
+++ src/ejercicio-3/cliente-servidor/client.ts
@@ -13,24 +13,41 @@
 import net from 'net';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
-import { FunkoCollection } from '../funko/funko-collection.js';
 import { Funko } from '../funko/funko.js';
 import { TiposFunko } from '../funko/funko.js';
 import { GeneroFunko } from '../funko/funko.js';
+import {EventEmitter} from 'events';
 
 export type RequestType = {
   type: 'add' | 'update' | 'remove' | 'read' | 'list';
+  user?: string
   funkoPop?: Funko[]
+  id?: number
 }
 
-export class Client {
+export class Client extends EventEmitter {
   private client: net.Socket;
   private commands: string[];
 
   constructor() {
-    this.add();
+    super();
     this.client = net.connect({port: 60300});
     this.commands = hideBin(process.argv);
+    this.add();
+    this.list();
+    this.read();
+    this.update();
+    this.remove();
+    let wholeData = '';
+    this.client.on('data', (dataChunk) => {
+      wholeData += dataChunk;
+      let messageLimit = wholeData.indexOf('\n');
+      while (messageLimit !== -1) { 
+        wholeData = wholeData.substring(0, messageLimit);
+        this.emit('response', JSON.parse(wholeData));
+        messageLimit = wholeData.indexOf('\n');
+      }
+    });
   }
 
   add() {
@@ -40,130 +57,284 @@ export class Client {
         description: 'User',
         type: 'string',
         demandOption: true
+      },
+      id: {
+       description: 'Funko ID',
+       type: 'number',
+       demandOption: true
+      },
+      name: {
+        description: 'Funko name',
+        type: 'string',
+        demandOption: true
+      },
+      desc: {
+        description: 'Funko description',
+        type: 'string',
+        demandOption: true
+      }, 
+      type: {
+        description: 'Funko type',
+        type: 'string',
+        demandOption: true
+      },
+      genre: {
+        description: 'Funko genre',
+        type: 'string',
+        demandOption: true
+      }, 
+      fr: {
+        description: 'Funko franchise',
+        type: 'string',
+        demandOption: true
+      },
+      frn: {
+        description: 'Funko franchise number',
+        type: 'number',
+        demandOption: true
+      },
+      ex: {
+        description: 'Funko exclusive',
+        type: 'boolean',
+        demandOption: true
+      },
+      sf: {
+        description: 'Funko special features',
+        type: 'string',
+        demandOption: true
+      },
+      vl: {
+        description: 'Funko value',
+        type: 'number',
+        demandOption: true
       }
-    //   id: {
-    //    description: 'Funko ID',
-    //    type: 'number',
-    //    demandOption: true
-    //   },
-    //   name: {
-    //     description: 'Funko name',
-    //     type: 'string',
-    //     demandOption: true
-    //   },
-    //   desc: {
-    //     description: 'Funko description',
-    //     type: 'string',
-    //     demandOption: true
-    //   }, 
-    //   type: {
-    //     description: 'Funko type',
-    //     type: 'string',
-    //     demandOption: true
-    //   },
-    //   genre: {
-    //     description: 'Funko genre',
-    //     type: 'string',
-    //     demandOption: true
-    //   }, 
-    //   fr: {
-    //     description: 'Funko franchise',
-    //     type: 'string',
-    //     demandOption: true
-    //   },
-    //   frn: {
-    //     description: 'Funko franchise number',
-    //     type: 'number',
-    //     demandOption: true
-    //   },
-    //   ex: {
-    //     description: 'Funko exclusive',
-    //     type: 'boolean',
-    //     demandOption: true
-    //   },
-    //     type: 'string',
-    //     demandOption: true
-    //   },
-    //   vl: {
-    //     description: 'Funko value',
-    //     type: 'number',
-    //     demandOption: true
-    //   } sf: {
-    //     description: 'Funko special features',
-    //     type: 'string',
-    //     demandOption: true
-    //   },
-    //   vl: {
-    //     description: 'Funko value',
-    //     type: 'number',
-    //     demandOption: true
-    //   }
      }, (argv) => {
-      const funkoCollection = new FunkoCollection(argv.user);
-      // let type: TiposFunko = TiposFunko.POP;
-      // switch (argv.type) {
-      //   case 'POP!':
-      //     type = TiposFunko.POP;
-      //     break;
-      //   case 'POP! Rides':
-      //     type = TiposFunko.POPRIDES;
-      //     break;
-      //   case 'Vynil Soda':
-      //     type = TiposFunko.VYNILSODA;
-      //     break;
-      //   case 'Vynil Gold':
-      //     type = TiposFunko.VYNILGOLD;
-      //     break;
-      // }
+      let type: TiposFunko = TiposFunko.POP;
+      switch (argv.type) {
+        case 'POP!':
+          type = TiposFunko.POP;
+          break;
+        case 'POP! Rides':
+          type = TiposFunko.POPRIDES;
+          break;
+        case 'Vynil Soda':
+          type = TiposFunko.VYNILSODA;
+          break;
+        case 'Vynil Gold':
+          type = TiposFunko.VYNILGOLD;
+          break;
+      }
     
-      // let genre: GeneroFunko = GeneroFunko.ANIMACION;
-      // switch (argv.genre) {
-      //   case 'Animación':
-      //     genre = GeneroFunko.ANIMACION;
-      //     break;
-      //   case 'Peliculas':
-      //     genre = GeneroFunko.PELICULAS;
-      //     break;
-      //   case 'TV':
-      //     genre = GeneroFunko.TV;
-      //     break;
-      //   case 'Videojuegos':
-      //     genre = GeneroFunko.VIDEOJUEGOS;
-      //     break;
-      //   case 'Deportes':
-      //     genre = GeneroFunko.DEPORTES;
-      //     break;
-      //   case 'Música':
-      //     genre = GeneroFunko.MUSICA;
-      //     break;
-      //   case 'Anime':
-      //     genre = GeneroFunko.ANIME;
-      //     break;
-      // }
-      
-      //funkoCollection.addFunko(new Funko(argv.id, argv.name, argv.desc, type, genre, argv.fr, argv.frn, argv.ex, argv.sf, argv.vl));
-      this.sendRequest(JSON.stringify({'type': 'add'}));
-    })
+      let genre: GeneroFunko = GeneroFunko.ANIMACION;
+      switch (argv.genre) {
+        case 'Animación':
+          genre = GeneroFunko.ANIMACION;
+          break;
+        case 'Peliculas':
+          genre = GeneroFunko.PELICULAS;
+          break;
+        case 'TV':
+          genre = GeneroFunko.TV;
+          break;
+        case 'Videojuegos':
+          genre = GeneroFunko.VIDEOJUEGOS;
+          break;
+        case 'Deportes':
+          genre = GeneroFunko.DEPORTES;
+          break;
+        case 'Música':
+          genre = GeneroFunko.MUSICA;
+          break;
+        case 'Anime':
+          genre = GeneroFunko.ANIME;
+          break;
+      }
+    
+      const new_funko = new Funko(argv.id, argv.name, argv.desc, type, genre, argv.fr, argv.frn, argv.ex, argv.sf, argv.vl);
+      const request: RequestType = {'type': 'add', 'user': argv.user, 'funkoPop': [new_funko]};
+      this.sendRequest(request)
+     })
+     .help()
+     .argv;
+  
+  }
+
+  list() {
+    yargs(this.commands)
+      .command('list', 'List all user Funkos', {
+      user: {
+       description: 'user',
+       type: 'string',
+       demandOption: true
+      }
+     }, (argv) => {
+      const request: RequestType = {'type': 'list', 'user': argv.user};
+      this.sendRequest(request)
+     })
      .help()
      .argv;
   }
 
-  sendRequest(request:  string) {
-    this.client.write(request);
+  update() {
+
+    yargs(this.commands)
+     .command('update', 'Update a Funko', {
+     user: {
+       description: 'User',
+       type: 'string',
+       demandOption: true
+     },
+     id: {
+      description: 'Funko ID',
+      type: 'number',
+      demandOption: true
+     },
+     name: {
+       description: 'Funko name',
+       type: 'string',
+       demandOption: true
+     },
+     desc: {
+       description: 'Funko description',
+       type: 'string',
+       demandOption: true
+     }, 
+     type: {
+       description: 'Funko type',
+       type: 'string',
+       demandOption: true
+     },
+     genre: {
+       description: 'Funko genre',
+       type: 'string',
+       demandOption: true
+     }, 
+     fr: {
+       description: 'Funko franchise',
+       type: 'string',
+       demandOption: true
+     },
+     frn: {
+       description: 'Funko franchise number',
+       type: 'number',
+       demandOption: true
+     },
+     ex: {
+       description: 'Funko exclusive',
+       type: 'boolean',
+       demandOption: true
+     },
+     sf: {
+       description: 'Funko special features',
+       type: 'string',
+       demandOption: true
+     },
+     vl: {
+       description: 'Funko value',
+       type: 'number',
+       demandOption: true
+     }
+    }, (argv) => {
+     let type: TiposFunko = TiposFunko.POP;
+     switch (argv.type) {
+       case 'POP!':
+         type = TiposFunko.POP;
+         break;
+       case 'POP! Rides':
+         type = TiposFunko.POPRIDES;
+         break;
+       case 'Vynil Soda':
+         type = TiposFunko.VYNILSODA;
+         break;
+       case 'Vynil Gold':
+         type = TiposFunko.VYNILGOLD;
+         break;
+     }
+   
+     let genre: GeneroFunko = GeneroFunko.ANIMACION;
+     switch (argv.genre) {
+       case 'Animación':
+         genre = GeneroFunko.ANIMACION;
+         break;
+       case 'Peliculas':
+         genre = GeneroFunko.PELICULAS;
+         break;
+       case 'TV':
+         genre = GeneroFunko.TV;
+         break;
+       case 'Videojuegos':
+         genre = GeneroFunko.VIDEOJUEGOS;
+         break;
+       case 'Deportes':
+         genre = GeneroFunko.DEPORTES;
+         break;
+       case 'Música':
+         genre = GeneroFunko.MUSICA;
+         break;
+       case 'Anime':
+         genre = GeneroFunko.ANIME;
+         break;
+     }
+     const new_funko = new Funko(argv.id, argv.name, argv.desc, type, genre, argv.fr, argv.frn, argv.ex, argv.sf, argv.vl);
+      const request: RequestType = {'type': 'update','user': argv.user, 'funkoPop': [new_funko]};
+      this.sendRequest(request)
+    })
+    .help()
+    .argv;
+  }
+
+  read() {
+    yargs(this.commands)
+     .command('read', 'Show al information of a Funko', {
+     user: {
+      description: 'user',
+      type: 'string',
+      demandOption: true
+     },
+     id: {
+       description: 'Funko ID',
+       type: 'number',
+       demandOption: true,
+     }
+    }, (argv) => {
+      const request: RequestType = {'type': 'read', 'user': argv.user, 'id': argv.id};
+      this.sendRequest(request)
+    })
+    .help()
+    .argv;
+ 
+  }
+
+  remove() {
+    yargs(this.commands)
+    .command('remove', 'Remove a funko', {
+    user: {
+     description: 'user',
+     type: 'string',
+     demandOption: true
+    },
+    id: {
+      description: 'Funko ID',
+      type: 'number',
+      demandOption: true,
+    }
+   }, (argv) => {
+    const request: RequestType = {'type': 'remove', 'user': argv.user, 'id': argv.id};
+    this.sendRequest(request)
+   })
+   .help()
+   .argv;
+  }
+
+  sendRequest(request:  RequestType) {
+    this.client.write(JSON.stringify(request) + '\n');
   }
 
 }
 
 const cliente = new Client();
-cliente.sendRequest(JSON.stringify({'type': 'add'}));
 
-// const client = net.connect({port: 60300});
 
-// client.on('data', (dataJSON) => {
-//   const message = JSON.parse(dataJSON.toString());
-
-//   if (message.type === 'established') {
-//     console.log(`Conexión correcta`);
-
-//   } 
-// })
+cliente.on('response', (request) => {
+  console.log(`Respuesta recibida ${request.type}`);
+});
--- src/ejercicio-3/cliente-servidor/server.ts
+++ src/ejercicio-3/cliente-servidor/server.ts
@@ -11,8 +11,10 @@
  */
 
 import net from 'net';
-import {watchFile} from 'fs';
+import {EventEmitter} from 'events';
 import { Funko } from '../funko/funko.js';
+import { RequestType } from './client.js';
+import { FunkoCollection } from '../funko/funko-collection.js';
 
 export type ResponseType = {
   type: 'add' | 'update' | 'remove' | 'read' | 'list';
@@ -21,20 +23,65 @@ export type ResponseType = {
 }
 
 
+export class Server extends EventEmitter {
+  constructor() {
+    super();
+    net.createServer((connection) => {
+      console.log('A client has connected.');
 
-  net.createServer((connection) => {
-    console.log('A client has connected.');
+      connection.write(JSON.stringify({'type': 'established', 'success': true}) +'\n');
 
-    connection.write(JSON.stringify({'type': 'established', 'success': 'true'}) +'\n');
+      let wholeData = '';
+      connection.on('data', (dataChunk) => {
+        wholeData += dataChunk;
+        let messageLimit = wholeData.indexOf('\n');
+        while (messageLimit !== -1) { 
+          wholeData = wholeData.substring(0, messageLimit);
+          this.emit('request', JSON.parse(wholeData), connection);
+          messageLimit = wholeData.indexOf('\n');
+        }
+      });
 
-    connection.on('data', (request) => {
-      const message = JSON.parse(request.toString());
-      console.log(`Petición recibida ${message.type}`);
+      connection.on('close', () => {
+        console.log('A client has disconnected.');
+      });
+    }).listen(60300, () => {
+      console.log('Waiting for clients to connect.');
     });
+  }
 
-    connection.on('close', () => {
-      console.log('A client has disconnected.');
-    });
-  }).listen(60300, () => {
-    console.log('Waiting for clients to connect.');
-  });
+  manageAddRequest(request: RequestType, conecction: net.Socket) {
+    const userName = request.user as string;
+    const collection = new FunkoCollection(userName);
+  }
+  manageUpdateRequest(request: RequestType, conecction: net.Socket) {
+    //
+  }
+  manageListRequest(request: RequestType, conecction: net.Socket) {
+    //
+  }
+  manageRemoveRequest(request: RequestType, conecction: net.Socket) {
+    //
+  }
+  manageReadRequest(request: RequestType, conecction: net.Socket) {
+    //
+  }
+}
+
+const server_ = new Server();
+
+server_.on('request', (request, connection) => {
+  const client_request = request as RequestType;
+  console.log(`Petición recibida ${client_request.type}`);
+  if(client_request.type === "add") {
+    server_.manageAddRequest(client_request, connection);
+  } else if (client_request.type === "update") {
+    server_.manageUpdateRequest(client_request, connection);
+  } else if(client_request.type === "list") {
+    server_.manageListRequest(client_request, connection);
+  } else if (client_request.type === "remove") {
+    server_.manageRemoveRequest(client_request, connection);
+  } else if(client_request.type === "read") {
+    server_.manageReadRequest(client_request, connection);
+  }
+});
